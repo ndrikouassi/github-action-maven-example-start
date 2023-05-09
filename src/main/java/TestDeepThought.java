bbd1b3bc8fbec1b7e1002f9f8dac3e927cb743f4
akouassi <akouassi@akouass-ltmpr8d.internal.salesforce.com>
2023-05-09 11:49:21 +0100

    test

--- src/main/java/TestDeepThought.java
+++ src/main/java/TestDeepThought.java
@@ -3,11 +3,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 
+
 public class TestDeepThought {
     public TestDeepThought() {
 
     }
         @Test
+        
         @DisplayName("Test the answer to everything")
         void testAnswerToAll(){
             DeepThought superComputer = new DeepThought();
